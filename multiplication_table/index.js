267ea67e5fcbebafef487d4d46abed0fc73d8844
JOHN CARLOS PAGADUAN (GEDI-ISD-OOCLL/MNL) <PAGADJO@OOCL.COM>
2020-10-01 13:40:59 +0800

    added index.js files and corresponding html files

---
+++ multiplication_table/index.js
@@ -0,0 +1,7 @@
+for(var x=1; x<10; x++){
+  for(var y=1; y<x+1; y++){
+    var product = x * y;
+    document.write(y +"*" + x + "="+ product + " | ")
+  }
+  document.write("<br>");
+}
---
+++ number/index.js
@@ -0,0 +1,10 @@
+
+for(var x=1; x <= 20; x++){
+  if(x%2==0){
+    console.log(x + " is even number")
+    document.write(x + " is even number<br>")
+  }else{
+    console.log(x + " is odd number")
+    document.write(x + " is odd number<br>");
+  }
+} 
